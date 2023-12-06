5a389a7f0600fd6c1cc20f5def37eabd23f68e24
HELDER RUIZ MARCELINO <110069838+helderruiz@users.noreply.github.com>
2023-12-06 17:57:38 -0300

    Feito o projeto da lâmpada
    Nesse projeto pode ligar a lâmpada, desligar e quebrar com 2 cliques em cima.

---
+++ lampada.js
@@ -0,0 +1,38 @@
+const ligar = document.querySelector('.ligar')
+const desligar = document.querySelector('.desligar')
+let imagem = document.querySelector('.imagem')
+const restaurar = document.querySelector('.restaurar')
+
+
+ligar.addEventListener('click', ()=> {
+    if (!lampadaQuebrada()){
+        imagem.setAttribute('src','/img/ligada.jpg' )
+    }else{
+        alert('Lâmpada Quebrada não Liga Zé')
+
+    }
+})
+
+desligar.addEventListener('click', function(){
+    if (!lampadaQuebrada()){
+        imagem.setAttribute('src', '/img/desligada.jpg')
+    }else{
+        alert('Lâmpada Quebrada também não Desliga Zé')
+    }
+})
+
+imagem.addEventListener('dblclick', ()=> {
+    imagem.setAttribute('src', '/img/quebrada.jpg')
+    
+})
+
+restaurar.addEventListener('click', ()=> {
+    location.reload()
+})
+
+function lampadaQuebrada(){
+    return imagem.src.indexOf ('quebrada') > -1
+    //indexOf retorna true ou false
+}
+
+
